760f19521edea4a7a0181c59d83601cc286b2c7f
Владимир <first.oxe@gmail.com>
2022-01-23 01:39:32 +0300

    add load logging middleware

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,7 +1,9 @@
 from aiogram import Dispatcher
 
 from .throttling import ThrottlingMiddleware
+from .logging import LoggingMiddleware
 
 
 def setup(dp: Dispatcher):
     dp.middleware.setup(ThrottlingMiddleware())
+    dp.middleware.setup(LoggingMiddleware())
