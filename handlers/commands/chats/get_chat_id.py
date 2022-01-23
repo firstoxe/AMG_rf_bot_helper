648dd6e0ab52dcc17e707ab60b761b04757be179
Владимир <first.oxe@gmail.com>
2022-01-24 01:19:47 +0300

    pep8

--- handlers/commands/chats/get_chat_id.py
+++ handlers/commands/chats/get_chat_id.py
@@ -2,6 +2,7 @@ from aiogram import types
 from loader import dp
 from utils.misc import rate_limit
 
+
 @rate_limit(3, 'get_chat_id')
 @dp.message_handler(commands='get_chat_id')
 async def bot_get_chat_id(message: types.Message):
